9d33eda5f8ef17fc69955b77205c473aa9068974
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-11-30 13:24:28 -0800

    Setup project boilerplate. Setup index.html, styles.css, and script.js. Made sure everything is connected and working together.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calc-2
+
+Taking another pass at a caclulator to show my front-end mastery!
---
+++ script.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+const h1 = document.createElement("h1");
+h1.innerText = "Hello World!";
+
+body.appendChild(h1);
