bbdc63738584455504bf2448fd8f2a8e13773e09
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-11-30 13:29:18 -0800

    Setup 4 math operators. Operators all set. Moving on to step 2

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const h1 = document.createElement("h1");
 h1.innerText = "Hello World!";
 
 body.appendChild(h1);
+
+const add = function (x, y) {
+  return x + y;
+};
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const multiply = function (x, y) {
+  return x * y;
+};
+
+const divide = function (x, y) {
+  return x / y;
+};
