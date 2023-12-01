f47c1dce4d4f454f4a6d292de922195424dd4149
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-11-30 16:01:55 -0800

    Finished calc input for nums. Added onclick to all num keys and clear key. Clicking num/clear updates display accordingly. Added some logic for leading zeros and max digit.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const add = function (x, y) {
+let display = document.querySelector(".display");
+
+function add(x, y) {
   return x + y;
-};
+}
 
-const subtract = function (x, y) {
+function subtract(x, y) {
   return x - y;
-};
+}
 
-const multiply = function (x, y) {
+function multiply(x, y) {
   return x * y;
-};
+}
 
-const divide = function (x, y) {
+function divide(x, y) {
   return x / y;
-};
+}
 
-const operate = function (operator, x, y) {
+function operate(operator, x, y) {
   switch (operator) {
     case "add":
       return add(x, y);
@@ -25,4 +27,18 @@ const operate = function (operator, x, y) {
     case "divides":
       return divide(x, y);
   }
-};
+}
+
+function calcInput(value) {
+  if (display.textContent.length > 9 && value !== "clear") {
+    display.textContent = "ERROR";
+  } else if (value === "clear") {
+    display.textContent = "";
+  } else {
+    if (display.textContent === "0") {
+      display.textContent = value;
+    } else {
+      display.textContent += value;
+    }
+  }
+}
